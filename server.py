54be8abb2e758a5288f7b7d78a4829379320dcd9
glenzhang2008 <glenzhang2008@hotmail.com>
2018-01-17 22:05:43 +0800

    add first file

---
+++ server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+from wsgiref.simple_server import make_server
+
+
+
+from hello import application
+
+
+httpd = make_server('', 8086, application)
+
+print("Serving HTTP on port 8086...")
+
+
+httpd.serve_forever()
